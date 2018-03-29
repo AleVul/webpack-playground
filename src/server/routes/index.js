8dedf6a42bbac7aad97f85732d2964e5e46832f3
AleVul <alex@lebo.md>
2018-03-29 17:22:34 +0300

    fixed some bugs

--- src/server/routes/index.js
+++ src/server/routes/index.js
@@ -8,9 +8,7 @@ const router = express.Router();
 
 router.get("/", (req, res, next) =>
     getUsers()
-        .then(users =>
-            res.sendFile(path.join(__dirname, "views", "index.html"))
-        )
+        .then(users => res.sendFile(path.join(__dirname, "views", "index.html")))
         .catch(next)
 );
 
